1402e7d60d250b3955dfe0d33f1c96bbf2eb8130
TruongQToan <toanbkhn95@gmail.com>
2018-10-12 00:41:17 +0700

    Change num epochs

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ import numpy as np
 import sys
 from utils import load_data, preprocess_data
 from rnn_rbm import RNNRBM
-import matplotlib.pyplot as plt
+import pandas as pd
+#import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -13,12 +14,16 @@ if __name__ == '__main__':
     X_test, y_test = \
         preprocess_data(test_data, window_side=5)
     model = RNNRBM(learning_rate=1e-5, visible_dim=1, \
-        hidden_dim=300, rnn_hidden_dim=300, num_epochs=4)
+        hidden_dim=300, rnn_hidden_dim=300, num_epochs=100)
     print ("Start training")
     preds, y, mae = model.train(X_train, y_train)
     print ("Finish training")
-    plt.plot(y, 'b-', label='True values')
-    plt.plot(preds, 'r-', label='Predicted values')
-    plt.legend(loc='upper left')
-    plt.title('Prediction {} \nMAE:{} \n'.format(dev.upper(), str(mae)))
-    plt.show()
+
+    train_results = list(zip(y, preds))
+    df = pd.DataFrame(data=train_results, columns=['Y', 'Predictions'])
+    df.to_csv('train_results.csv', index=False, header=False)
+    # plt.plot(y, 'b-', label='True values')
+    # plt.plot(preds, 'r-', label='Predicted values')
+    # plt.legend(loc='upper left')
+    # plt.title('Prediction {} \nMAE:{} \n'.format(dev.upper(), str(mae)))
+    # plt.show()
